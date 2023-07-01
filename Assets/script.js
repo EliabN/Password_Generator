1666fd2c0a24094f8ee6c1f8c06f918e892e4684
eliabn <30064396@tafe.wa.edu.au>
2023-07-01 22:52:48 +0800

    First criteria

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var password = [];
+
+function generatePassword() {
+
+  var lowerCase = window.confirm("Would you like to include lowercase");
+
+
+  if (lowerCase) { 
+    var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    var index = Math.floor(Math.random() * alphabetLower.length);
+    password = (password + alphabetLower[index]);
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
