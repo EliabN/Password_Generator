ace38f11eca2ca8f929218b794da848e7c95ef97
eliabn <30064396@tafe.wa.edu.au>
2023-07-01 23:35:42 +0800

    All criteria statements done

--- Assets/script.js
+++ Assets/script.js
@@ -7,12 +7,39 @@ function generatePassword() {
 
   var lowerCase = window.confirm("Would you like to include lowercase");
 
+  var upperCase = window.confirm("Would you like to include uppercase");
+
+  var numeric = window.confirm("Would you like to include numeric");
+
+  var specialChar = window.confirm("Would you like to include special characters");
+
 
   if (lowerCase) { 
     var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     var index = Math.floor(Math.random() * alphabetLower.length);
     password = (password + alphabetLower[index]);
+  }
+
+  if (upperCase) {
+    var alphabetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    var index = Math.floor(Math.random() * alphabetUpper.length);
+    password = (password + alphabetUpper[index]);
+  }
+
+  if (numeric) {
+    var numb = [0,1,2,3,4,5,6,7,8,9];
+
+    var index = Math.floor(Math.random() * numb.length);
+    password = (password + numb[index]);
+  }
+
+  if (specialChar) { 
+    var spCharacters = ["!","#","$","%","&","(",")","*","+","-","/",":",";","<","=",">","?","@","[","\"","]","^","_","`","{","|","}","~",",","'","\"",")"]
+    
+    var index = Math.floor(Math.random() * spCharacters.length);
+    password = (password + spCharacters[index]);
   }
 }
 
